525836209defca4b3b3639c48212eed79e20c456
Karen Grigoryan <karengrig@mail.ru>
2015-04-02 16:36:14 +0300

    some changes

--- creaturesDeclarations.js
+++ creaturesDeclarations.js
@@ -1,3 +1,9 @@
+
+
+
+
+
+
 function Dot(x,y,color){
 
 this.x=x;
@@ -12,10 +18,12 @@ this.nextStepPoint = new Point(x,y);
 
 if (color == "blue"){
 this.run = "flee";
+this.numberOfPossibleSteps = 3;
 this.calculateNextStep = nextStepBlueDot;
 }else{
 this.run = "chase";
-this.calculateNextStep = nextStepBlueDot;
+this.numberOfPossibleSteps = 10;
+this.calculateNextStep = nextStepRedDot;
 }
 
 
@@ -33,54 +41,22 @@ this.draw = drawDot;
 }
 
 
-/*
-function nextStepDotFleeOrChase(){
-	this.nextStepPoint = this.randomDisplacementWithRadius(10);
-	if (this.arrayOfCounterparts.length>0){
-		var indexOfClosest = this.findClosest(this.arrayOfCounterparts);
-		var distance = findDistance(this,this.arrayOfCounterparts[indexOfClosest]);
-		if (distance<50){
-			var possibleSteps = [];
-			var indexOfNextStepPoint = 0;
-			for (var i = 0;i<10;i++){
-				var possiblePoint = this.randomDisplacementWithRadius(10);
-				possibleSteps.push(possiblePoint);
-			}
-		
-		    if (this.run == "flee"){
-		   		indexOfNextStepPoint = this.arrayOfCounterparts[indexOfClosest].findMostDistant(possibleSteps);
-		    else if (this.run == "chase"){
-				indexOfNextStepPoint = this.arrayOfCounterparts[indexOfClosest].findClosest(possibleSteps);
-		   }
-		}
-		
-			this.nextStepPoint = possibleSteps[indexOfNextStepPoint];
-		
 
-	}
-    }
-}
-*/
 
 
 
 
 function nextStepBlueDot(){
+	var arrayOfCounterparts = redDots;
+	
 	this.nextStepPoint = this.randomDisplacementWithRadius(10);
-	if (redDots.length>0){
-		var indexOfClosestPredator = this.findClosest(redDots);
-		var distance = findDistance(this,redDots[indexOfClosestPredator]);
+	
+	if (arrayOfCounterparts.length>0){
+		var closestPredator = arrayOfCounterparts[this.findClosest(arrayOfCounterparts)];
+		var distance = findDistance(this,closestPredator);
 
 		if (distance<50){
-			var possibleSteps = [];
-			for (var i = 0;i<10;i++){
-				var possiblePoint = this.randomDisplacementWithRadius(10);
-				possibleSteps.push(possiblePoint);
-			}
-
-			var indexOfNextStepPoint = redDots[indexOfClosestPredator].findMostDistant(possibleSteps);
-
-			this.nextStepPoint = possibleSteps[indexOfNextStepPoint];
+			this.nextStepPoint = nextStepForObjectFleeOrChase(this,"runs away from",closestPredator);
 		}
 
 	}
@@ -88,59 +64,40 @@ function nextStepBlueDot(){
 
 
 function nextStepRedDot(){
+	var arrayOfCounterparts = blueDots;
 	this.nextStepPoint = this.randomDisplacementWithRadius(10);
-	if (blueDots.length>0){
-		var indexOfTarget = this.findClosest(blueDots);
-		var possibleSteps = [];
-		for (var i = 0;i<10;i++){
-			var possiblePoint = this.randomDisplacementWithRadius(10);
-			possibleSteps.push(possiblePoint);
-		}
-
-		var indexOfNextStepPoint = blueDots[indexOfTarget].findClosest(possibleSteps);
-		this.nextStepPoint = possibleSteps[indexOfNextStepPoint];
+	if (arrayOfCounterparts.length>0){
+		
+		var closestTarget = arrayOfCounterparts[this.findClosest(arrayOfCounterparts)];
+		this.nextStepPoint = nextStepForObjectFleeOrChase(this,"chases",closestTarget);
+		
 	}
 }
 
 
-Dot.prototype.randomDisplacementWithRadius = function(radius){
-	var x = this.x + getRandomArbitrary(-Math.abs(radius), Math.abs(radius));
-	var y = this.y + getRandomArbitrary(-Math.abs(radius), Math.abs(radius));
-	if (!bodyIsWithinBoundaries(x,10,500)) x = this.x;
-	if (!bodyIsWithinBoundaries(y,10,500)) y = this.y;
-
-	var nextStepPoint = new Point(x,y);
-	return nextStepPoint;
+function nextStepForObjectFleeOrChase(obj,fleeOrChase,counterPartObj){
+	var possibleSteps = obj.generateRandomPossibleStepsWithNumberAndRadius(obj.numberOfPossibleSteps,10);
+	var indexOfNextStepPoint;
+	
+	switch (fleeOrChase){
+	case "runs away from":
+		indexOfNextStepPoint = counterPartObj.findMostDistant(possibleSteps);
+		break;
+	case "chases":
+		indexOfNextStepPoint = counterPartObj.findClosest(possibleSteps);
+		break;
+	
+	}
+	
+	
+	return possibleSteps[indexOfNextStepPoint];
 }
 
 
-Dot.prototype.findClosest = function(arrayOfOtherObjects){
-	var indexOfClosest = -1;
-	var minDistance = 1000000;
-	var thisObject = this;
-	arrayOfOtherObjects.forEachItem(function(array,i){
-		var distance = findDistance(thisObject,array[i]);
-		if (distance<minDistance){
-			minDistance = distance;
-			indexOfClosest = i;
-		 }
- 
-	});
-	return indexOfClosest;
-}
 
-Dot.prototype.findMostDistant = function(arrayOfOtherObjects){
-var indexOfMostDistant = -1;
-var maxDistance = 0;
-var thisObject = this;
-arrayOfOtherObjects.forEachItem(function(array,i){
- var distance = findDistance(thisObject,array[i]);
- if (distance>maxDistance){
- 	maxDistance = distance;
- 	indexOfMostDistant = i;
- }
- 
-});
-return indexOfMostDistant;
-}
 
+
+Dot.prototype.generateRandomPossibleStepsWithNumberAndRadius = generateRandomPossibleStepsWithNumberAndRadius;
+Dot.prototype.randomDisplacementWithRadius = randomDisplacementWithRadius;
+Dot.prototype.findClosest = findClosest;
+Dot.prototype.findMostDistant = findMostDistant;
--- helpfulFunctions.js
+++ helpfulFunctions.js
@@ -21,3 +21,57 @@ function findDistance(object1,object2){
 function bodyIsWithinBoundaries(coordinate,radius,dimentionOfCanvas){
 	return coordinate>radius&&coordinate<dimentionOfCanvas-radius
 }
+
+
+
+function findMostDistant(arrayOfOtherObjects){
+var indexOfMostDistant = -1;
+var maxDistance = 0;
+var thisObject = this;
+arrayOfOtherObjects.forEachItem(function(array,i){
+ var distance = findDistance(thisObject,array[i]);
+ if (distance>maxDistance){
+ 	maxDistance = distance;
+ 	indexOfMostDistant = i;
+ }
+ 
+});
+return indexOfMostDistant;
+}
+
+
+function generateRandomPossibleStepsWithNumberAndRadius(number,radius){
+	var possibleSteps = [];
+		for (var i = 0;i<number;i++){
+			var possiblePoint = this.randomDisplacementWithRadius(radius);
+			possibleSteps.push(possiblePoint);
+		}
+	return possibleSteps;
+}
+
+
+function randomDisplacementWithRadius(radius){
+	var x = this.x + getRandomArbitrary(-Math.abs(radius), Math.abs(radius));
+	var y = this.y + getRandomArbitrary(-Math.abs(radius), Math.abs(radius));
+	if (!bodyIsWithinBoundaries(x,10,500)) x = this.x;
+	if (!bodyIsWithinBoundaries(y,10,500)) y = this.y;
+
+	var nextStepPoint = new Point(x,y);
+	return nextStepPoint;
+}
+
+
+function findClosest(arrayOfOtherObjects){
+	var indexOfClosest = -1;
+	var minDistance = 1000000;
+	var thisObject = this;
+	arrayOfOtherObjects.forEachItem(function(array,i){
+		var distance = findDistance(thisObject,array[i]);
+		if (distance<minDistance){
+			minDistance = distance;
+			indexOfClosest = i;
+		 }
+ 
+	});
+	return indexOfClosest;
+}
